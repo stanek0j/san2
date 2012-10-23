342d52a83f1456984e98a372e49fc09592056dfa
Pavel Slechta <slechta@email.cz>
2012-10-23 18:54:11 +0200

    CProducerConsumer: try_push(); try_pop() add

--- utils/cproducerconsumer.hpp
+++ utils/cproducerconsumer.hpp
@@ -25,7 +25,7 @@ namespace San2
 			private:
 				unsigned long m_maxQueueSize;
 				std::queue<T, Container> m_que;
-				std::mutex m_mutex; // This is necessary becaouse of multiple Prod. a Cons.
+				std::mutex m_mutex; // This is necessary because of MULTIPLE producers and consumers
 				std::condition_variable m_condNotEmpty;
 				std::condition_variable m_condNotFull;
 				
@@ -54,34 +54,51 @@ namespace San2
 				}
 
 				template <class Rep, class Period>
+				int try_push(const T &item, CThread *thr, std::chrono::duration<Rep, Period> duration)
+				{
+					std::unique_lock<std::mutex> lock(m_mutex);
+					
+					while(isFull())
+					{
+						if (m_condNotEmpty.wait_for(lock, duration) == std::cv_status::timeout)	return -2;
+						if (thr->isTerminated()) return -1;
+					}
+					
+					if (thr->isTerminated()) return -1;
+					m_que.push(item);
+					m_condNotFull.notify_all();
+					return 0;
+				}
+
+				template <class Rep, class Period>
 				int push(const T &item, CThread *thr, std::chrono::duration<Rep, Period> duration)
 				{
 					std::unique_lock<std::mutex> lock(m_mutex);
-					//while(isFull()) m_condNotEmpty.wait(lock);
-					
-					while(isFull()) // dokud je plna cekame
+					while(isFull())
 					{
-						// tohle vraci true	pokud nenastal time
-						// tohle vraci predikat, kdyz nastal timeout
-						// chceme aby predikat byl false, kdyz thread byl terminovan
-						
 						m_condNotEmpty.wait_for(lock, duration);
-						if (thr->isTerminated())
-						{
-							printf("terminated1\n");
-							return -1; // thread was terminated		
-						}
+						if (thr->isTerminated()) return -1;
 					}
-					
-					if (thr->isTerminated()) 
-					{
-						printf("terminated2\n");
-						return -1; // thread was terminated
-					}
-					
-						m_que.push(item);
+					if (thr->isTerminated()) return -1;
+					m_que.push(item);
 					m_condNotFull.notify_all();
+					return 0;
+				}
+
+				template <class Rep, class Period>
+				int try_pop(T *out, CThread *thr, std::chrono::duration<Rep, Period> duration)
+				{
+					std::unique_lock<std::mutex> lock(m_mutex);				
+				    while (isEmpty())
+				    { 
+						if (m_condNotFull.wait_for(lock, duration) == std::cv_status::timeout) return -2;
+						if (thr->isTerminated()) return -1; 
+					}
+					if (thr->isTerminated()) return -1; 
 					
+					*out = m_que.front();
+					m_que.pop();
+					m_condNotEmpty.notify_all();					
 					return 0;
 				}
 
@@ -92,22 +109,12 @@ namespace San2
 				    while (isEmpty())
 				    { 
 						m_condNotFull.wait_for(lock, duration);
-						
-						if (thr->isTerminated())
-						{
-							printf("terminated3\n");
-							return -1; // thread was terminated		
-						}
+						if (thr->isTerminated()) return -1;
 					}
 					
-					if (thr->isTerminated()) 
-					{
-						printf("terminated4\n");
-						return -1; // thread was terminated
-					}
-					
-						*out = m_que.front();
-						m_que.pop();
+					if (thr->isTerminated()) return -1;
+					*out = m_que.front();
+					m_que.pop();
 					m_condNotEmpty.notify_all();					
 					return 0;
 				}
