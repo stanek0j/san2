458a6d48d66ac285f15d97c5e8e88a0cdf1e6ac5
Pavel Slechta <slechta@email.cz>
2012-11-29 15:40:35 +0100

    add missing file from previous commit atomic.hpp

---
+++ utils/atomic.hpp
@@ -0,0 +1,48 @@
+
+#pragma once
+#include <mutex>
+
+namespace San2
+{
+	namespace Utils
+	{
+		template <class T>
+		class Atomic
+		{
+			public:
+				Atomic()
+				{
+					
+				}
+			
+				constexpr Atomic(T value) :
+					m_variable(value)
+				{
+					
+				}
+			
+				T operator=(T value)
+				{
+					std::lock_guard<std::mutex> lock(m_mutexVariable);
+					m_variable = value;
+					return m_variable;
+				}
+				
+				operator T()
+				{
+					std::lock_guard<std::mutex> lock(m_mutexVariable);
+					return m_variable;	
+				}	
+				
+			private:
+				std::mutex m_mutexVariable;
+				T m_variable;
+			
+			#ifndef MSVC
+				Atomic& operator=(const Atomic&)=delete;
+				Atomic& operator=(const Atomic&)volatile=delete;
+			#endif
+		};
+	}	
+	
+}
